8590b562622b2e970a2a12014309030984445f52
SpceForMind <casha_davydov@mail.ru>
2018-08-16 00:37:23 +0500

    wsgi_application

--- Nginx/web/hello.py
+++ Nginx/web/hello.py
@@ -4,7 +4,7 @@
     <environ> is kwarg variables of environment
     <start_request> is function for send status_code and headers
 '''
-def wsgi_application(environ, start_request):
-   # start_request("200 OK", [("Content-Type", "text/plain")])
+def wsgi_application(environ, start_response):
+    start_response("200 OK", [("Content-Type", "text/plain")])
     body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     return body
