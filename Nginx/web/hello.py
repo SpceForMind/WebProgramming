c6f41d217b372c406f444a2b0697e7964b550bf2
SpceForMind <casha_davydov@mail.ru>
2018-08-16 16:40:03 +0500

    wsgi_app, nginx.conf

--- Nginx/web/hello.py
+++ Nginx/web/hello.py
@@ -3,9 +3,8 @@
     Wsgi application
     <environ> is kwarg variables of environment
     <start_request> is function for send status_code and headers
-    Using version of python -2.7(requier in stepik terminal)
 '''
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    body = [bytes(i + '\n') for i in environ['QUERY_STRING'].split('&')]
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     return body
