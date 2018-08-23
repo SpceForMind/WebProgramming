9a50ffd2bc177596c4a4bd1e1bbfd44cc0038b07
SpceForMind <casha_davydov@mail.ru>
2018-08-23 18:34:05 +0000

    add Question, Answer models and QuestionManager

--- Nginx/web/ask/qa/models.py
+++ Nginx/web/ask/qa/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#Manager of model <Question>
+class QuestionManager(models.Manager):
+	'''
+		sort by add date
+	'''
+	def new(self):
+		return self.order_by("-added_at")
+	'''
+		sort by rating
+	'''
+	def popular(self):
+		return self.order_by("-rating")
+
+class Question(models.Model):
+	'''
+		field object for access to methods of manager - Question.objects.<method>
+	'''
+	objects = QuestionManager()
+	title = models.CharField(max_length = 255)
+	text = models.TextField()
+	added_at = models.DateTimeField(blank = True, auto_now_add = True)
+	rating = models.IntegerField(default = 0)	
+	'''
+		<n questions> : <1 user>
+	'''
+	author = models.ForeignKey(User)
+	'''
+		<n questions> : <m users>
+		likes is list of users 
+		related_name set in other value for avoidong error
+		creation question_set for reverse relation
+	'''
+	likes = models.ManyToManyField(User, related_name = "likes_set")
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateTimeField(blank = True, auto_now_add = True)
+	'''
+		<n answers> : <1 question>
+	'''
+	question = models.ForeignKey(Question)
+	'''
+		<n answers> : <1 author>
+	'''
+	author = models.ForeignKey(User)
