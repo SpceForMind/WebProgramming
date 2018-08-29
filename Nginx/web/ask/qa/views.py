9508ee882262bdfdb59280a2d08a6e8a0b691760
SpceForMind <casha_davydov@mail.ru>
2018-08-29 13:49:58 +0000

    views.py, templates/...html

--- Nginx/web/ask/qa/views.py
+++ Nginx/web/ask/qa/views.py
@@ -1,5 +1,33 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response, render
+from qa import models
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator
+from django.http import Http404
 
-#simple controller
-def test(request, *args, **kwargs):
-	return HttpResponse('OK')
+'''
+	Main page
+	/?page=<num_page>
+'''
+#question per page
+limit = 10
+
+def list_new_questions(request):
+	questions = models.Question.objects.new()
+	try:
+		page = int(request.GET.get('page', 1))
+	except ValueError:
+		raise Http404
+	paginator = Paginator(questions, limit)
+	paginator.base_url = "/question/"
+	page = paginator.page(page)
+	return render_to_response(
+		'list_new_questions.html',
+		'page': page,
+		'paginator': paginator,
+		'quesitons': page.object_list,
+	)
+
+
+
+
